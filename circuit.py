f6ea9a7cbb85d64e8f2aba199c2b2a653a558460
dmittrey <dmitryzubakhin@mail.ru>
2023-01-09 03:52:57 +0300

    Add circuit lib

---
+++ circuit.py
@@ -0,0 +1,52 @@
+from typing import Dict, List, Set, TypeVar, Generic, List
+from abc import ABC, abstractmethod
+from numpy import int16
+
+T = TypeVar('T', int16, bool)
+
+
+class WireCircuitComponent(Generic[T]):
+    def __init__(self) -> None:
+        self.__value: T = 0
+        pass
+
+    def receive_value(self, value: T) -> None:
+        self.__value = value
+        pass
+
+    def get_value(self) -> T:
+        return self.__value
+
+
+class FunctionalCircuitComponent(ABC):
+    def __init__(self, registers: List[str], inputs: List[str]) -> None:
+        self.__registers: Dict[str, int16] = {i: 0 for i in registers}
+
+        self.__inputs: List[str] = inputs
+
+        self.__pipes: Dict[str, WireCircuitComponent[int16]] = dict()
+
+        self.__signals: Dict[str, WireCircuitComponent[bool]] = dict()
+
+    @abstractmethod
+    def do_tick(self) -> None:
+        pass
+
+    def receive_value(self, register_name: str, value: int16) -> None:
+        assert register_name in self.__registers, 'Указанный регистр не существует'
+        self.__registers[register_name] = value
+        pass
+
+    def get_value(self, register_name: str) -> int16:
+        assert register_name in self.__registers, 'Указанный регистр не существует'
+        return self.__registers[register_name]
+
+    def attach_pipe(self, register_name: str, pipe: WireCircuitComponent[int16]) -> None:
+        assert register_name in self.__registers, 'Указанный регистр не существует'
+        self.__pipes[register_name] = pipe
+        pass
+
+    def attach_signal(self, input_name: str, signal: WireCircuitComponent[bool]) -> None:
+        assert input_name in self.__inputs, 'Указанный выход не существует'
+        self.__signals[input_name] = signal
+        pass
