3cb43b70c0a48b9c10254002924194547dd2cf71
dmittrey <dmitryzubakhin@mail.ru>
2023-01-09 19:44:53 +0300

    Add dict constraints and expand signal size

--- circuit.py
+++ circuit.py
@@ -1,8 +1,8 @@
 from typing import Dict, List, Set, TypeVar, Generic, List
 from abc import ABC, abstractmethod
-from numpy import int16
+from numpy import int16, int8
 
-T = TypeVar('T', int16, bool)
+T = TypeVar('T', int16, int8)
 
 
 class WireCircuitComponent(Generic[T]):
@@ -21,46 +21,56 @@ class WireCircuitComponent(Generic[T]):
 class FunctionalCircuitComponent(ABC):
     def __init__(self, registers: List[str], inputs: List[str]) -> None:
         self.__registers: Dict[str, int16] = {i: 0 for i in registers}
-        self.__inputs: Dict[str, bool] = {i: 0 for i in inputs}
+        self.__inputs: Dict[str, int8] = {i: 0 for i in inputs}
 
         self.__pipes: Dict[str, WireCircuitComponent[int16]] = dict()
-        self.__signals: Dict[str, WireCircuitComponent[bool]] = dict()
+        self.__signals: Dict[str, WireCircuitComponent[int8]] = dict()
 
     @abstractmethod
     def do_tick(self) -> None:
-        self.__refresh_state()
         pass
 
     def attach_pipe(self, register_name: str, pipe: WireCircuitComponent[int16]) -> None:
         assert register_name in self.__registers.keys(), 'Указанный регистр не существует'
-        self.__pipes[register_name] = pipe
+        self.__pipes.update(register_name=pipe)
         pass
 
     def attach_signal(self, input_name: str, signal: WireCircuitComponent[bool]) -> None:
         assert input_name in self.__inputs.keys(), 'Указанный выход не существует'
-        self.__signals[input_name] = signal
+        self.__signals.update(input_name=signal)
         pass
 
     def receive_value(self, register_name: str) -> None:
         assert register_name in self.__registers.keys(), 'Указанный регистр не существует'
-        self.__registers[register_name] = self.__pipes[register_name].get_value()
+
+        pipe = self.__pipes.get(register_name)
+        if (pipe != None):
+            self.__registers[register_name] = pipe.get_value()
+
         pass
 
     def set_value(self, register_name: str, val: int16) -> None:
         assert register_name in self.__registers.keys(), 'Указанный регистр не существует'
-        self.__registers[register_name] = val
-        self.__pipes[register_name].receive_value(val)
+        self.__registers.update(register_name=val)
+
+        pipe = self.__pipes.get(register_name)
+        if (pipe != None):
+            pipe.receive_value(val)
         pass
 
     def receive_signal(self, input_name: str) -> None:
         assert input_name in self.__inputs.keys(), 'Указанный выход не существует'
-        self.__inputs[input_name] = self.__signals[input_name].get_value()
+
+        signal = self.__signals.get(input_name)
+        if (signal != None):
+            self.__inputs[input_name] = signal.get_value()
+
         pass
 
     def __get_value(self, register_name: str) -> int16:
         assert register_name in self.__registers.keys(), 'Указанный регистр не существует'
         return self.__registers[register_name]
 
-    def __get_signal(self, input_name: str) -> bool:
+    def __get_signal(self, input_name: str) -> int8:
         assert input_name in self.__inputs.keys(), 'Указанный выход не существует'
         return self.__inputs[input_name]
