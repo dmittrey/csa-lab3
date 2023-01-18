27e8814e161cba13b752cb342382b837f8f070d1
dmittrey <dmitryzubakhin@mail.ru>
2023-01-18 03:01:50 +0300

    Change hello.asm code to ISA

--- examples/hello.asm
+++ examples/hello.asm
@@ -10,37 +10,37 @@ d: 'd'
 
 section .text
 _start:
-ld AC, h
+ld AC, +h(ZR)
 sw AC, +1(ZR)   ; Save symbol to output device(cell #1)
 
-ld AC, e
+ld AC, +e(ZR)
 sw AC, +1(ZR)
 
-ld AC, l
+ld AC, +l(ZR)
 sw AC, +1(ZR)
 
-ld AC, l
+ld AC, +l(ZR)
 sw AC, +1(ZR)
 
-ld AC, o
+ld AC, +o(ZR)
 sw AC, +1(ZR)
 
-ld AC, space
+ld AC, +space(ZR)
 sw AC, +1(ZR)
 
-ld AC, w
+ld AC, +w(ZR)
 sw AC, +1(ZR)
 
-ld AC, o
+ld AC, +o(ZR)
 sw AC, +1(ZR)
 
-ld AC, r
+ld AC, +r(ZR)
 sw AC, +1(ZR)
 
-ld AC, l
+ld AC, +l(ZR)
 sw AC, +1(ZR) 
 
-ld AC, d
+ld AC, +d(ZR)
 sw AC, +1(ZR)
 
 halt
