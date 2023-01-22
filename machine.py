c2587ac012f44628430e8ecfd0455a7ad7e008d3
dmittrey <dmitryzubakhin@mail.ru>
2023-01-23 00:02:06 +0300

    Refactor methods in ControlUnit
    Separate complex method's logic in several methods to easily testing

--- machine.py
+++ machine.py
@@ -1,15 +1,14 @@
 from enum import Enum
 import sys
-from typing import Dict, List, List
-from numpy import int16, int8
+from typing import Dict, List
 from circuit import CircuitComponent
-from components import Register, SignExpand, Trigger, Memory, RegisterFile, ALU, MUX, IOHandler, IOMemoryCell
+from components import SignExpand, Trigger, Memory, RegisterFile, ALU, MUX, IOHandler
 
 from isa import read_code, Opcode
 
 
 class DataPath():
-    class DataPathComponent(CircuitComponent, Enum):
+    class DataPathComponent(Enum):
         PC = Trigger()
         Adr_Src_Mux = MUX(1, 'AdrSrc')
         Memory = Memory(512)
@@ -42,14 +41,6 @@ class DataPath():
         for component in self.DataPathComponent.list():
             component.enter_interrupt()
 
-    def __get_info(self, tick: int) -> Dict[str, int]:
-        print(str(tick), ')', sep='', end=' ')
-        for register_name, register_value in self.register_file.inner_registers.items():
-            print('x' + str(register_name), register_value, sep=' : ', end=' | ')
-        print('\nAluSrcA :', self.alu.get_value('srcA'), '|', 'ALUSrcB :', self.alu.get_value(
-            'srcB'), '|', 'Result :', self.alu.get_value('Result'), '|', sep=' ', end=' ')
-        print('PC :', self.pc_triger.state)
-
 
 class ControlUnit(CircuitComponent):
 
@@ -70,7 +61,7 @@ class ControlUnit(CircuitComponent):
         super().__init__(['OPCODE', 'PCWrite', 'AdrSrc', 'MemWrite', 'IRWrite', 'WDSrc', 'IOOp',
                           'ImmSrc', 'ALUControl', 'ALUSrcB', 'ALUSrcA', 'RegWrite', 'Zero', 'IOInt'])
 
-    def start(self, data_path: DataPath) -> None:
+    def start(self, data_path: DataPath = None) -> None:
         while True:
             opcode = self._wires['OPCODE'].get()
             if (opcode == Opcode.HALT):
@@ -95,6 +86,31 @@ class ControlUnit(CircuitComponent):
                     self._registers[wire_name] = wire.get()
                     pass
 
+    def _save_context(self) -> Dict[str, int]:
+        # Set interrupt mode and receive INT signal
+        self._in_interrupt_context = True
+        self.set_register('IOInt', 0)
+
+        # Save control unit state
+        return self._registers.copy()
+
+    def _restore_context(self, registers: Dict[str, int]) -> None:
+        # Restore state after handling interrupt
+        self._registers = registers
+        self._in_interrupt_context = False
+
+    def __handle_interrupt(self, data_path: DataPath) -> None:
+        if (self.__is_interrupts_allowed and (not self._in_interrupt_context)
+                and self.get_register('IOInt') == 1):
+            registers = self._save_context()
+
+            # Goto interrupt vector
+            data_path.enter_interrupt()
+            # Start executing
+            self.start(data_path)
+
+            self._restore_context(registers)
+
     def __change_valves(self, new_state: Dict[str, int]) -> None:
         for register_name, register_val in self._registers.items():
             if (new_state.get(register_name) != None):
@@ -105,24 +121,6 @@ class ControlUnit(CircuitComponent):
 
     def __get_op_transitions(self, op: int) -> None | List[Dict[str, int]]:
         return self._instruction_transitions.get(op)
-
-    def __handle_interrupt(self, data_path: DataPath) -> None:
-        if (self.__is_interrupts_allowed and (not self._in_interrupt_context)
-                and self.get_register('IOInt') == 1):
-            # Set interrupt mode and receive INT signal
-            self._in_interrupt_context = True
-            self.set_register('IOInt', 0)
-
-            # Save control unit state
-            registers = self._registers.copy()
-            # Goto interrupt vector
-            data_path.enter_interrupt()
-            # Start executing
-            self.start(data_path)
-
-            # Restore state after handling interrupt
-            self._registers = registers
-            self._in_interrupt_context = False
 
 
 def main(args):
